cbf72122b0480f0fbd6b99ab3efca2eccee780f9
skinnyidiot <dknew@ptoronmail.com>
2022-01-30 14:10:52 +0530

    button group display experiment

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -7,16 +7,17 @@ function display()
 
 let button = document.getElementsByClassName('num');
 
+console.log(button[0].innerHTML);
 
+/*
 for (var i = 0; i < button.length; i++)
 {
 
     button[i].addEventListener('click',function()
 	{
-	    let result = button[i];
-	    let result = button[i].innerHTML;
+	    let result = button[i.innerHTML;
 	    console.log(result);
 	}
     )
 }
-
+*/
